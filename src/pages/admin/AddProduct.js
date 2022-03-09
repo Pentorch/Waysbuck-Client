036cd3eb0fdad420f59a3aef57a775e063e6f426
WillyAgustinoEfendi <59289325+Pentorch@users.noreply.github.com>
2022-03-09 20:30:14 +0700

    update

--- src/pages/admin/AddProduct.js
+++ src/pages/admin/AddProduct.js
@@ -51,7 +51,7 @@ const AddDataProduct = () => {
       const formData = new FormData();
       formData.set("tittle", form.tittle);
       formData.set("price", form.price);
-      formData.set("image", preview);
+      formData.set("image", formData.image[0], formData.image[0].name);
 
       console.log(form);
 
